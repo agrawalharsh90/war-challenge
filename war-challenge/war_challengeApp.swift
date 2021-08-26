c91b044bb2066a5c1eeafbd72a1ebca89615523c
Harsh Agrawal <agrawalharsh90@gmail.com>
2021-08-26 07:26:42 +0530

    Initial Commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Harsh Agrawal on 26/08/21.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
